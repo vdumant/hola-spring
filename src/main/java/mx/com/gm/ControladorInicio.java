2b382d8929df4436b44fb7e4eea6acbec026b8ed
vdumant <duman.fabian.g@gmail.com>
2024-10-23 16:57:27 -0400

    first commit lesson 2

--- src/main/java/mx/com/gm/ControladorInicio.java
+++ src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,18 +1,43 @@
 package mx.com.gm;
 
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
-    public String inicio() {
-        log.info("ejecutando controlador...");
-        log.debug("Mas detalle del controlador...");
-        return "Hola mundo con string";
+    public String inicio(Model model) {
+        var mensaje = "Hola mundo con thymeleaf";
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("juan@mail.com");
+        persona.setTelefono("123456789");
+
+        var persona2 = new Persona();
+        persona2.setNombre("Luis");
+        persona2.setApellido("Calle");
+        persona2.setEmail("luis@mail.com");
+        persona2.setTelefono("0987654321");
+
+        var personas = Arrays.asList();
+
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        model.addAttribute("personas", personas);
+
+        log.info("ejecutando controlador Spring MVC");
+        return "index";
     }
 
 }
---
+++ src/main/java/mx/com/gm/domain/Persona.java
@@ -0,0 +1,12 @@
+package mx.com.gm.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
